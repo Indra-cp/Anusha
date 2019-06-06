67b16c8624d3296294c6de06b4cbb9901079c11d
Indra-cp <51438210+Indra-cp@users.noreply.github.com>
2019-06-06 14:38:40 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int a,b,sum;
+void getinput()
+{
+  pritf("enter the two  number");
+ scanf("%d%d",&a,&b);
+}
+  int add()
+   {
+      sum=a+b;
+      return sum;
+    }
+
+       void print()
+        {
+          void getinput();
+          int add();
+          printf("the values of 2 num %d",sum);
+        }
+           
+
+          void main()
+           {
+             void print();
+            }
+
+  
+
+   
